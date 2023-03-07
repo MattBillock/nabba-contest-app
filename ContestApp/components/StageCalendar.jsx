a210b95f04dda7e8fb3485171e3163d8b4848e2d
Matt Billock <matt@billock.org>
2023-03-07 01:01:42 -0600

    Added ability to click into a scheduled event and see details

--- ContestApp/components/StageCalendar.jsx
+++ ContestApp/components/StageCalendar.jsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import { Dimensions, SafeAreaView, ScrollView, SectionList, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Dimensions, ImageBackground, SafeAreaView, ScrollView, SectionList, StyleSheet, TouchableOpacity, View } from "react-native";
 import Constants from 'expo-constants';
 import { Agenda } from 'react-native-calendars';
 
-import { Card, Text, useTheme } from 'react-native-paper';
+import { Button, Card, DataTable, Divider, IconButton, Provider, Text, useTheme } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import { selectBandList, selectContestDates, selectDefaultVenue, selectPerformancesByStage, selectPerformanceSchedule, selectPerformancesForStage, selectVenueList } from '../features/contestData/contestDataSlice';
-
+import createStyle from '../app/styles';
 
 export function StageCalendar(props) {
 
+  const [activeSlot, setActiveSlot] = React.useState(undefined);
+  const [activeBand, setActiveBand] = React.useState(undefined);
   const theme=useTheme();
 
+  //const styles = createStyle();
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -36,6 +39,14 @@ export function StageCalendar(props) {
       marginVertical:1,
       borderRadius: 5,
       height:48
+    },
+    cardNoHeight: {
+      backgroundColor: theme.colors.secondaryContainer,
+      color: theme.colors.secondary,
+      opacity: 1,
+      borderWidth: 1,
+      marginVertical:1,
+      borderRadius: 5,
     },
     sectionHeader: {
       paddingTop: 20,
@@ -100,33 +111,41 @@ export function StageCalendar(props) {
     }
   }
 
-  function getAgenda(itemList) {
-    return (
-      <Agenda
-        // The list of items that have to be displayed in agenda. If you want to render item as empty date
-        // the value of date key has to be an empty array []. If there exists no value for date key it is
-        // considered that the date in question is not yet loaded
-        items={performanceSchedule[venue]}
-        // Initially selected day
-        selected={'2023-04-21'}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={'2023-04-21'}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={'2023-04-22'}
-        // Max amount of months allowed to scroll to the past. Default = 50
-        pastScrollRange={0}
-        // Max amount of months allowed to scroll to the future. Default = 50
-        futureScrollRange={0}
-        rowHasChanged={(r1, r2) => {
-          return r1.band_draw !== r2.band_draw;
-        }}
-        // Specify how each item should be rendered in agenda
-        
-
-      />
-    );
+  function setActiveItems(band, slot) {
+    setActiveSlot(slot);
+    setActiveBand(band);
   }
-  let content = getAgenda(props.performanceSchedule);
+
+  const button = <Button
+    icon="close"
+    mode="elevated"
+    onPress={() => setActiveItems(undefined, undefined)}
+  >
+    <Text variant="titleSmall">Go back</Text>
+  </Button>;
+
+  function addCalendarEven(band, slot) {
+    const eventConfig = {
+      title: getBandName(band.name, slot.item.band_draw, slot.item.title),
+      startDate: slot.item.performance_times.start_timestamp,
+      description: ""
+      // and other options
+    };
+     
+    AddCalendarEvent.presentEventCreatingDialog(eventConfig)
+      .then((eventInfo) => {
+        // handle success - receives an object with `calendarItemIdentifier` and `eventIdentifier` keys, both of type string.
+        // These are two different identifiers on iOS.
+        // On Android, where they are both equal and represent the event id, also strings.
+        // when { action: 'CANCELED' } is returned, the dialog was dismissed
+        console.warn(JSON.stringify(eventInfo));
+      })
+      .catch((error) => {
+        // handle error such as when user rejected permissions
+        //console.warn(error);
+      });
+  }
+
   let moment = require('moment');
   //apiCalendar.listUpcomingEvents(100).then(({result}:any) => {
   //  setContestEvents(result)
@@ -146,31 +165,76 @@ export function StageCalendar(props) {
     })
     return result_array;
   }
-  return (
-      <SectionList
-        stickySectionHeadersEnabled={false}
-        scrollEnabled={true}
-        sections={buildListFormat()} 
-        renderItem={(item) => {
-          let band = getBandDetailsById(item.item.band_id);
-          let band_name = getBandName(band.name, item.item.band_draw, item.item.title)
-          const options = { hour: "numeric", minute: "2-digit" };
-          let start_time = new Date(item.item.performance_times.start_timestamp)
-          return (
-            //<TouchableOpacity>
-              <Card elevated style={styles.card}>
-                <Card.Title title={moment(start_time).format('LT') + " - " + band_name} titleNumberOfLines={2} />
-                
-              </Card>
-              
-            //</TouchableOpacity>
-            );  
-        }} 
-        renderSectionHeader={({section}) => (
-          <Text style={styles.sectionHeader}  variant="displayMedium">{section.title}</Text>
-        )}
-        keyExtractor={item => `basicListEntry-${item.band_draw}`} 
-        />
+
+  let content;
+  if(activeSlot && activeBand && activeBand.name != "not found") {
+    let pieces = activeBand.choice_pieces.filter(entry => entry.performance_slot === activeSlot.item.band_draw)
     
+    let piece_info = <Card elevated style={styles.cardNoHeight} onPress={() => setActiveItems(undefined,undefined)}>
+      <Card.Title title={getBandName(activeBand.name, activeSlot.item.band_draw, activeSlot.item.title)} />
+      <Card.Title title={"Event time: " + moment(activeSlot.item.performance_times.start_timestamp).format('LT')} />
+      <Card.Content>
+        <Divider />
+        <Text>&nbsp;</Text>
+        <Text>Pieces performed:</Text>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>Piece</DataTable.Title>
+            <DataTable.Title>Composer</DataTable.Title>
+          </DataTable.Header>
+            { pieces.map(element => { 
+              return <DataTable.Row key={element.title + '-' + element.composer}>
+                <DataTable.Cell>{element.title}</DataTable.Cell>
+                <DataTable.Cell>{element.composer}</DataTable.Cell>
+              </DataTable.Row>
+            })}
+        </DataTable>
+      </Card.Content>
+      {button}
+    </Card>
+    content = piece_info
+  }
+  else if(activeSlot && activeSlot.item.title && activeBand && activeBand.name == "not found") {
+    content = <Card elevated style={styles.cardNoHeight} onPress={() => setActiveItems(undefined,undefined)}>
+    <Card.Title title={activeSlot.item.title} />
+    <Card.Content>
+      <Text>Start time: {moment(activeSlot.item.performance_times.start_timestamp).format('LT')}</Text>
+      <Text>&nbsp;</Text>
+      <Divider />
+      <Text>&nbsp;</Text>
+    </Card.Content>
+    {button}
+  </Card>
+  }
+  else {
+    content = <SectionList
+      stickySectionHeadersEnabled={false}
+      scrollEnabled={true}
+      sections={buildListFormat()} 
+      renderItem={(item) => {
+        let band = getBandDetailsById(item.item.band_id);
+        let band_name = getBandName(band.name, item.item.band_draw, item.item.title)
+        let start_time = new Date(item.item.performance_times.start_timestamp)
+        return (
+          <TouchableOpacity>
+            <Card elevated style={styles.card} onPress={() => setActiveItems(band,item)}>
+              <Card.Title title={moment(start_time).format('LT') + " - " + band_name} titleNumberOfLines={2} />
+            </Card>
+          </TouchableOpacity>
+          );  
+      }} 
+      renderSectionHeader={({section}) => (
+        <Text style={styles.sectionHeader}  variant="displayMedium">{section.title}</Text>
+      )}
+      keyExtractor={item => `basicListEntry-${item.band_draw}`} 
+      />
+  }
+  
+  return (
+    <Provider>
+      <ImageBackground source={require('../assets/background.jpg')} style={styles.image}>
+      {content}
+      </ImageBackground>
+    </Provider>
   );
 };
